7bead177d6d419aac05ea6059e860f09b9cf926a
victorchenbb <oqtwccho21778@outlook.com>
2025-11-28 17:31:00 +0800

    deploy contract

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedMysticAPR = await deploy("MysticAPR", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`MysticAPR contract: `, deployedMysticAPR.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_mysticAPR"; // id required to prevent reexecution
+func.tags = ["MysticAPR"];
